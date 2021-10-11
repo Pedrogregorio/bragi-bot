e28615637479a62873f895e9e7f3508ccb70cf62
Pedro Gregorio <pedrogregorio@jeraapps.com>
2021-10-11 15:04:38 -0400

    delete message embed duplicade

--- src/responses/play.js
+++ src/responses/play.js
@@ -1,8 +1,13 @@
 import { MessageEmbed } from 'discord.js';
 
 const playingMusic = async (server, title) => {
+  server.textChannel.messages.cache.forEach(msg => {
+    if (msg.author.username === 'bragi' && msg.embeds.length > 0) {
+      if (!msg.embeds[0].title) return msg.delete();
+    }
+  });;
   const playMusic = new MessageEmbed()
-    .setColor('#008486')
+    .setColor('#f28fa0')
     .setDescription(`Tocando a musica: **${title}** :musical_note:`)
   await server.textChannel.send({ embed: playMusic });
   return;
