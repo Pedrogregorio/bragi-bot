fca88bcd2e6c05b70cb9d350b780ed4b66f27983
Pedro Gregorio <pedrogregorio@jeraapps.com>
2021-10-28 07:55:16 -0400

    [FIX] crash next song

--- src/comands/clean.js
+++ src/comands/clean.js
@@ -1,7 +1,11 @@
 const clean = (message) => {
   const serverQueue = message.client.queue.get(message.guild.id);
-  if (serverQueue?.songs.length > 0) {
-    serverQueue.songs = [];
+  if (serverQueue?.songs.length > 1) {
+    serverQueue.songs.forEach((_, index) => {
+      if (index !== 0) {
+        serverQueue.songs.splice(index)
+      }
+    });
     return message.channel.send("Sua lista agora está vazia!"); 
   }
   return message.channel.send("Sua lista já está vazia!");
