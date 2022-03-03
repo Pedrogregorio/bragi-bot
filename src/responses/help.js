ee1baeb461fbfe2b7bd4a6b3309ed158aa5f85d8
Pedro Gregorio <pedrogregorio@jeraapps.com>
2022-03-02 21:54:32 -0400

    [FIX] help response

--- src/responses/help.js
+++ src/responses/help.js
@@ -1,10 +1,20 @@
 import { MessageEmbed } from 'discord.js';
 
 const helpCommands = async (server) => {
+  const commands =
+    [
+      "**`~play, ~p [URL musica/playlist Spotify]`** - Tocar musica",
+      "**`~skip`** - Pular musica",
+      "**`~list, ~queue`** - Listar fila de musicas",
+      "**`~clean`** - Limpar listar de musicas",
+      "**`~stop, ~quit, ~disconnect, ~leave`** - Parar o bot",
+      "**`~pause, ~resume`** - Pausar/Continuar musica",
+      "**`~loop`** - Definir musica como loop(Desenvolvimento)"
+    ];
   const playMusic = new MessageEmbed()
     .setColor('#62f8fb')
     .setTitle('Lista de comandos')
-    .setDescription(" `~play [URL musica/playlist Spotify]` - Tocar musica \n `~skip` - Pular musica \n `~list` - Listar fila de musicas \n `~clean` - Limpar listar de musicas \n `~stop` - Parar o bot \n ")
+    .setDescription(commands.join('\n'));
   await server.send({ embed: playMusic });
   return;
 }
